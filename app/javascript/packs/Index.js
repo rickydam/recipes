ddc22f1b3e714a4dc7b7e73763e5a77fd2abe55d
Ricky Dam <RickyDamOttawa@gmail.com>
2019-11-03 18:25:46 -0500

    Load App component with routes at application entry point

--- app/javascript/packs/Index.js
+++ app/javascript/packs/Index.js
@@ -2,25 +2,17 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React from "react";
+import { render } from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import App from "../components/App";
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+document.addEventListener("DOMContentLoaded", () => {
+    render(
+        <App />,
+        document.body.appendChild(document.createElement("div"))
+    );
+});
