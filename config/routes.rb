0bc2bbe92619e5afe5a83bec5464d1cd6ceb6660
Ricky Dam <RickyDamOttawa@gmail.com>
2019-11-03 19:24:42 -0500

    Generate recipes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      get 'recipes/create'
+      get 'recipes/show'
+      get 'recipes/destroy'
+    end
+  end
   root 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
