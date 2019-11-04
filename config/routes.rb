62313e19e53f307c33dbf16bd00fe9aea049e72e
Ricky Dam <RickyDamOttawa@gmail.com>
2019-11-03 19:32:44 -0500

    Fix routes for recipes actions: create, show, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'recipes/index'
-      get 'recipes/create'
-      get 'recipes/show'
-      get 'recipes/destroy'
+      post 'recipes/create'
+      get '/show/:id', to: 'recipes#show'
+      delete '/destroy/:id', to: 'recipes#destroy'
     end
   end
   root 'homepage#index'
