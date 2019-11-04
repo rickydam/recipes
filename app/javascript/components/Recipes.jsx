f6798399f5ec79d8c30663e53140228e7d6d1e58
Ricky Dam <RickyDamOttawa@gmail.com>
2019-11-03 20:57:40 -0500

    Initialize Recipes component

---
+++ app/javascript/components/Recipes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Recipes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            recipes: []
+        };
+    }
+}
+export default Recipes;
